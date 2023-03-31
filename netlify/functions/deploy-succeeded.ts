792ed1436873e472b6af8ad832beb7a45702647a
Dawid Gul <panjogurcik@outlook.com>
2023-03-31 18:13:32 +0200

    testing api

---
+++ netlify/functions/deploy-succeeded.ts
@@ -0,0 +1,36 @@
+import { Handler, HandlerContext, HandlerEvent } from "@netlify/functions"
+import axios from "axios"
+import { config } from "dotenv"
+config()
+
+const handler: Handler = async (
+     event: HandlerEvent,
+     context: HandlerContext
+) => {
+     try {
+          await axios.post(
+               `https://api.jgrtowy.xyz/netlify?key=${process.env.NETLIFY_KEY}`,
+               // `http://localhost:5000/netlify?key=${process.env.NETLIFY_KEY}`,
+               {
+                    eventType: "deploy-building",
+                    eventBody: JSON.parse(event.body || "{}"),
+               },
+               {
+                    headers: {
+                         "Content-Type": "Application/json",
+                    },
+               }
+          )
+     } catch (e) {
+          console.log(e)
+          return {
+               statusCode: 500,
+          }
+     }
+
+     return {
+          statusCode: 200,
+     }
+}
+
+export { handler }
